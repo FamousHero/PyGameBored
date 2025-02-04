f5809c4bd4b278f2f4b04fab032a54c3ab0e2e58
Daniel Franco <danielfranco2011@gmail.com>
2025-02-03 16:26:49 -0800

    Line movement

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ class Display(tk.Canvas):
         self.width = width
         self.height = height
         self.angle = 1
+        self.focus_set()
     
-    def rotate(self):
+    def move(self, event,offset_x, offset_y):
+        print("pressed sometihng: " + event.char)
+        x1,y1,x2,y2 = self.coords(self.line)
+        self.coords(self.line, x1+offset_x,y1+offset_y,x2+offset_x,y2+offset_y)
+        self.update()
+
+    def rotate(self, objectId, angle_deg):
         x1, y1, x2, y2 = self.coords(self.line)
-        angle_rad = math.radians(1)
+        angle_rad = math.radians(angle_deg)
         center_x, center_y = self.width/2, self.height/2
         x1, y1, x2, y2 = x1-center_x, y1-center_y, x2-center_x, y2-center_y
         new_x1 = x1*math.cos(angle_rad) - y1*math.sin(angle_rad)
@@ -23,9 +30,11 @@ class Display(tk.Canvas):
         new_y1, new_y2 = new_y1+center_y, new_y2+center_y
         self.coords(self.line, new_x1, new_y1, new_x2, new_y2)
         self.update()
-        if self.angle <= 360:
+        if self.angle < 360:
             self.angle += 1
-            self.after(20, self.rotate)
+            self.after(20, self.rotate, self.line, 1)
+    
+
 
 
 if __name__ == "__main__":
@@ -34,5 +43,12 @@ if __name__ == "__main__":
     window.rowconfigure(0,weight=1)
     canvas = Display(window, width=1000, height=1000, background='gray75')
     canvas.grid(column=0, row=0)
-    window.after(1000, canvas.rotate)
+    window.after(1000, canvas.rotate, canvas.line, 1)
+    # Rotating around center of screen, not line
+    # moving during rotation will affect final position
+    canvas.bind("d", lambda x: canvas.move(x,3,0))
+    canvas.bind("w", lambda x: canvas.move(x,0,-3))
+    canvas.bind("a", lambda x: canvas.move(x,-3,0))
+    canvas.bind("s", lambda x: canvas.move(x,0,3))
+
     window.mainloop()
